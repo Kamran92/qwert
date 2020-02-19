a1d5f8fd9d6fe17392e294699a4bf2e3b6461b5c
Kamran92 <k.r.a.92mail.ru>
2020-02-19 16:10:23 +0300

    Updates

---
+++ precache-manifest.b121b34019f6f77bc3a781334f093317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6d3f061710684ceaf72668a887f4bd",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "78cc1cb158ee2dfb39d7",
+    "url": "/star-db/static/css/main.444b21fb.chunk.css"
+  },
+  {
+    "revision": "6a74a8925685dcd0c04f",
+    "url": "/star-db/static/js/2.da695755.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/star-db/static/js/2.da695755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78cc1cb158ee2dfb39d7",
+    "url": "/star-db/static/js/main.4dc02607.chunk.js"
+  },
+  {
+    "revision": "5077f6bbb0916d81b20d",
+    "url": "/star-db/static/js/runtime-main.c28b7452.js"
+  }
+]);
